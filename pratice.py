17a308d58f379ad18c81da0013ab1f791a47ff14
Kimseongjin <mansj98@naver.com>
2022-03-14 19:26:18 +0900

    again python

---
+++ pratice.py
@@ -0,0 +1,35 @@
+#정수형, 실수형
+print(5)
+print(-10)
+print(3.14)
+print(1000)
+print(5+3)
+print(2*8)
+print(3*(3+1))
+
+#문자열
+print("Balloon")
+print('Butterfly')
+print("hahahahahahahha")
+print("h" * 9)
+
+#Boolean 참 / 거짓
+print(5 > 10)
+print(5 < 10)
+print(True)
+print(False)
+print(not True)
+print(not False)
+print(not (5 > 10))
+
+#변수
+#애완동물
+animal = "강아지"
+name = "연탄이"
+age = 4
+hobby = "산책"
+is_adult = age >= 3
+
+print("우리집 " + animal + "의 이름은 " + name + "에요")
+print("연탄이는 " + str(age) + "살이며, " + hobby + "을 아주 좋아해요")
+print("연탄이는 어른일까요? " + str(is_adult))
